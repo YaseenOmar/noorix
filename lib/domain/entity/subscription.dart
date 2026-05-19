b55a616b6792dd24265482b7e3ff596ada87271a
Mohammed-qmr <mma.mqamar@gmail.com>
2026-05-19 13:17:09 +0300

    refactor: rename subscription file and format code

--- lib/domain/entity/subscription.dart
+++ lib/domain/entity/subscription.dart
@@ -1,17 +1,12 @@
-class SubscriptionEntity{
+class SubscriptionEntity {
   final String name;
   final String id;
   final String userId;
   final bool isActive;
   SubscriptionEntity({
-     required this.name,
+    required this.name,
     required this.id,
     required this.userId,
-    required this.isActive
-
-
-
-});
-
-
+    required this.isActive,
+  });
 }
